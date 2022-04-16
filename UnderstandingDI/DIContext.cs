bcc4994e5772cdc3adf638a1971f828f6ffbaeb5
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-04-16 15:16:08 -0400

    Translating DI Stage 5: Encapsulating the Context

---
+++ UnderstandingDI/DIContext.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+
+namespace UnderstandingDI
+{
+	public class DIContext
+	{
+		// the Dependency Injection Container in formal terms
+		// Spring refers to this as the ApplicationContext
+
+		private readonly ISet<object> serviceInstances = new HashSet<object>();
+
+		public DIContext(IEnumerable<Type> serviceClasses)
+		{
+			// step 1: create an instance of each service class
+			foreach (var serviceClass in serviceClasses)
+			{
+				// https://stackoverflow.com/questions/178645/how-does-wcf-deserialization-instantiate-objects-without-calling-a-constructor#179486
+				// https://stackoverflow.com/questions/390578/creating-instance-of-type-without-default-constructor-in-c-sharp-using-reflectio
+				serviceInstances.Add(FormatterServices.GetUninitializedObject(serviceClass));
+			}
+			
+			// step 2: wire them together
+			foreach (var serviceInstance in serviceInstances)
+			{
+				foreach (var property in serviceInstance.GetType().GetProperties())
+				{
+					var propertyType = property.PropertyType;
+					
+					foreach (var matchPartner in serviceInstances)
+					{
+						// https://stackoverflow.com/questions/708205/c-sharp-object-type-comparison
+						var matchPartnerType = matchPartner.GetType();
+						
+						// find a suitable matching service instance
+						if (IsAssignableTo(propertyType, matchPartnerType))
+						{
+							property.SetValue(serviceInstance, matchPartner);
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		public T GetServiceInstance<T>()
+		{
+			Type desiredType = typeof(T);
+			foreach (var serviceInstance in serviceInstances)
+			{
+				if (serviceInstance.GetType().Equals(desiredType))
+				{
+					return (T)serviceInstance;
+				}
+			}
+			
+			foreach (var serviceInstance in serviceInstances)
+			{
+				if (desiredType.IsAssignableFrom(serviceInstance.GetType()))
+				{
+					return (T)serviceInstance;
+				}
+			}
+
+			throw new ClassImplementationNotFoundException(desiredType);
+		}
+		
+		private bool IsAssignableTo(Type propertyType, Type instanceType)
+		{
+			if (propertyType == instanceType)
+			{
+				return true; // Either both are null or they are the same type
+			}
+
+			if (propertyType == null || instanceType == null)
+			{
+				return false;
+			}
+
+			if (propertyType.IsAssignableFrom(instanceType) ||
+			    instanceType.IsSubclassOf(propertyType))
+			{
+				return true;
+			}
+
+			return propertyType.BaseType == instanceType.BaseType; // They have the same immediate parent
+		}
+	}
+
+	public class ClassImplementationNotFoundException: Exception
+	{
+		public ClassImplementationNotFoundException(Type type) : 
+			base("Cannot locate an instance of type " + type.FullName)
+		{
+		}
+	}
+}
--- UnderstandingDI/Program.cs
+++ UnderstandingDI/Program.cs
@@ -9,80 +9,24 @@ namespace UnderstandingDI
 	{
 		static void Main(string[] args)
 		{
+			var context = CreateContext();
+			DoBusinessLogic(context);
+		}
+
+		private static DIContext CreateContext()
+		{
 			var serviceClasses = new HashSet<Type>();
 			serviceClasses.Add(typeof(ServiceA));
 			serviceClasses.Add(typeof(ServiceB));
-			
-			var serviceA = createServiceA(serviceClasses);
-			
+			return new DIContext(serviceClasses);
+		}
+
+		private static void DoBusinessLogic(DIContext context)
+		{
+			var serviceA = context.GetServiceInstance<IServiceA>();
+			var serviceB = context.GetServiceInstance<IServiceB>();
 			Console.WriteLine(serviceA.jobA());
-		}
-
-		private static ServiceA createServiceA(ISet<Type> serviceClasses)
-		{
-			// step 1: create an instance of each service class
-			var serviceInstances = new HashSet<object>();
-			foreach (var serviceClass in serviceClasses)
-			{
-				// https://stackoverflow.com/questions/178645/how-does-wcf-deserialization-instantiate-objects-without-calling-a-constructor#179486
-				// https://stackoverflow.com/questions/390578/creating-instance-of-type-without-default-constructor-in-c-sharp-using-reflectio
-				serviceInstances.Add(FormatterServices.GetUninitializedObject(serviceClass));
-			}
-			
-			// step 2: wire them together
-			foreach (var serviceInstance in serviceInstances)
-			{
-				foreach (var property in serviceInstance.GetType().GetProperties())
-				{
-					var propertyType = property.PropertyType;
-					
-					foreach (var matchPartner in serviceInstances)
-					{
-						// https://stackoverflow.com/questions/708205/c-sharp-object-type-comparison
-						var matchPartnerType = matchPartner.GetType();
-						
-						// find a suitable matching service instance
-						if (IsAssignableTo(propertyType, matchPartnerType))
-//						if (matchPartnerType.IsAssignableFrom(propertyType))
-						{
-							property.SetValue(serviceInstance, matchPartner);
-							break;
-						}
-					}
-				}
-			}
-			
-			// step 3: from all our service instances, find ServiceA
-			foreach (var serviceInstance in serviceInstances)
-			{
-				if (serviceInstance is ServiceA)
-				{
-					return serviceInstance as ServiceA;
-				}
-			}
-
-			return null;
-		}
-
-		public static bool IsAssignableTo(Type propertyType, Type instanceType)
-		{
-			if (propertyType == instanceType)
-			{
-				return true; // Either both are null or they are the same type
-			}
-
-			if (propertyType == null || instanceType == null)
-			{
-				return false;
-			}
-
-			if (propertyType.IsAssignableFrom(instanceType) ||
-			    instanceType.IsSubclassOf(propertyType))
-			{
-				return true;
-			}
-
-			return propertyType.BaseType == instanceType.BaseType; // They have the same immediate parent
+			Console.WriteLine(serviceB.jobB());
 		}
 	}
 }
